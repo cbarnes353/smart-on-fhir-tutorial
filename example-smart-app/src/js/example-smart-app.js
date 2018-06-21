4da9a20204884a01daa40f1dd001038daf92d4d6
Calvin Barnes <cbarnes353@hotmail.com>
2018-06-21 17:54:41 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -41,7 +41,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-
+          var other = byCodes('2089-1'); 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = gender;
