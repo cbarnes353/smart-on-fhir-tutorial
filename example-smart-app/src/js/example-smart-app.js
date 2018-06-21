1230deb992a349a77e7a044f3947b4ac0a863d84
Calvin Barnes <cbarnes353@hotmail.com>
2018-06-21 19:00:43 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -48,7 +48,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.other = patient.address;
+          p.other = patient.birthDate;
           
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
