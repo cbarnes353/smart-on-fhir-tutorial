663b009c40ca224146050c3bc46862b6f264d0e9
Calvin Barnes <cbarnes353@hotmail.com>
2019-03-07 10:04:11 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -36,14 +36,16 @@
           var fname = '';
           var lname = '';
           var mrn = '';
-          
+          var fin = '';
+          var location = '';
+          /*
           for (i = 0; i=patient.identifier.length ; i++) {
               if patient.identifier[i].system  = 'urn:oid:2.2.2.2.2.2'){
                 mrn = patient.identifier[i].value;
               }  
 
           }
-          
+          */
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
